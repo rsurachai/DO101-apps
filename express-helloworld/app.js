120cf008929ae7e93e57b3a9f4eac1cff094927e
Surachai Rahong <surachai.rahong@outlook.com>
2022-05-28 23:18:10 +0700

    Add hello olive

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/olive', function (req, res) {
+  res.send('Hello olive!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
